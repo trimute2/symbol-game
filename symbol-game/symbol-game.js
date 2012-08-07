65754a15f871c55fff007f29c8819a5b700f79ce
Ben Stern <ben.j.stern@gmail.com>
2012-08-07 18:42:55 -0400

    sixth commit

--- symbol-game/symbol-game.js
+++ symbol-game/symbol-game.js
@@ -1,65 +1,63 @@
-function easterEgg(){
+var Game ={
+};
+window.easterEgg = function(){
 
-	var map = [[' ',' ',' ',' ',' ',' ',' ',' ',' ',' ']
-			   [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ']
-			   [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ']
-			   ['_','_','_',' ',' ',' ',' ',' ',' ',' ']
-			   [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ']
-			   [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ']
-			   [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ']
-			   [' ',' ',' ',' ',' ',' ','_','_','_',' ']
-			   [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ']
-			   [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ']
-			   ['_','_','_','_','_','_','_','_','_','_']]
-	var game,
-	      data,
-	      dom,
-	      events,
-	      objects,
-	data ={
-		map[]
-		NODE_WIDTH
-		NODE_HEIGHT
-		world_width
-		world_height
-		world_cols
-		world_rows
-	}
-	game = {
-		data: data;
-		objects: objects;
-	}
-	objects ={
-		KeyBoardMonitor: null
-		platforms
-	}
+	var map = [[' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+				[' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+				[' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+				['_','_','_',' ',' ',' ',' ',' ',' ',' '],
+				[' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+				[' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+				[' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+				[' ',' ',' ',' ',' ',' ','_','_','_',' '],
+				[' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+				[' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+				['_','_','_','_','_','_','_','_','_','_']];
+		var data = {
+			map : [],
+			NODE_WIDTH : 0,
+			NODE_HEIGHT : 0,
+			world_width : 0,
+			world_height : 0,
+			world_cols : 0,
+			world_rows :0
+		};
+		var game = {
+			data: data,
+			objects: objects
+		};
+		var objects={
+			KeyBoardMonitor: null,
+			platforms : []
+		};
+
 	function platform(map){
-		data ={
-			platform [][]
-		}
+		var data ={
+			platform: []
+		};
 		for (var h = 0; h <= 10; h++) {
 			for (var w = 0; w >= 9; w++) {
-				if(map[h][w] === '_'){
-					platform[] += [w*5,h*5]
+				if(map[h][w] == '_'){
+					platform.push([w*5,h*5]);
 				}
-			};
-		};
+			}
+		}
 	}
 	function StickMan(X ,Y, W, H){
-		var data{
-		x: X,
-		y: Y,
-		w: W,
-		h: H,
-		collison: true,
-		falling: false,
-		jumping: false,
-		walking: false,
-		jumpPhase: 0,
-		direction{
-			left: true,
-			right: false
-		};
+		var data = {
+			x: X,
+			y: Y,
+			w: W,
+			h: H,
+			collison: true,
+			falling: false,
+			jumping: false,
+			walking: false,
+			jumpPhase: 0,
+			direction : {
+				left: true,
+				right: false
+			}
 		};
 		function moveTo(x, y){
 			data.x = x;
@@ -84,9 +82,9 @@ function easterEgg(){
 			data.walking = true;
 
 			if (xDirection > 0) {
-				addDirection('right','left')
+				addDirection('right','left');
 			}else if(xDirection < 0){
-				addDirection('left','right')
+				addDirection('left','right');
 			}
 		}
 		function gravity(){
@@ -94,13 +92,13 @@ function easterEgg(){
 				if(!jumping){
 					return 1;
 				}else{
-					jumpPhase--
+					jumpPhase--;
 					if (jumpPhase === 0) {
 						jumping = false;
-					};
+					}
 					return -1;
 				}
-			};
+			}
 		}
 		function jump(){
 			if(!jumping && !falling){
@@ -117,7 +115,7 @@ function easterEgg(){
 				}
 			}
 		}
-		function moveBy(x,y){
+		/*function moveBy(x,y){
 			var row, col,
 				w =game.data.NODE_WIDTH
 				h =game.data.NODE_HEIGHT
@@ -125,7 +123,7 @@ function easterEgg(){
 			targetY;
 			col = Math.min(game.data.world_cols, Math.max(0, data.col+x));
 			row = Math.min(game.data.world_rows, Math.max(0, data.row+y));
-		}
+		}*/
 		function animate(){
 			var k = game.objects.KeyBoardMonitor;
 
@@ -134,10 +132,10 @@ function easterEgg(){
 
 			if (k.isDown('left')) {
 				x = -1;
-			};
+			}
 			if (k.isDown('right')) {
 				x = 1;
-			};
+			}
 
 
 
@@ -147,141 +145,120 @@ function easterEgg(){
 		}
 	}
 	function KeyBoardMonitor(){
-		var keys,
-		events,
 
 		downKeys = {},
-		keyMap ={
-			'left': 37,
-	         'up': 38,
-	         'right': 39
-	     };
-
-	     events{
-
-	    	KeyDown: function(e){
-	     		if(keys[e.Keycode]&&keys[e.Keycode].down){ 
-	     			if (typeof downKeys[e.keyCode] === 'undefined') {
-	            		downKeys[e.keyCode] = true;
-	            		keys[e.keyCode].down(e);
-	          		}
-	          		if (typeof keys[e.keyCode].allowEvent === 'undefined') {
-	            		return stopEvent(e);
-	          		}
-	        	}
-
-	     	},
-	     	keyup: function(e) {
-
-	        	if (typeof downKeys[e.keyCode] !== 'undefined' && keys[e.keyCode]) {
-	          		delete downKeys[e.keyCode];
-	          		if (keys[e.keyCode].up) {
-	            		keys[e.keyCode].up(e);
-	          		}
-	          		if (typeof keys[e.keyCode].allowEvent === 'undefined') {
-	            		return stopEvent(e);
-	          		}
-	        	}
-
-	      	}
-
-	    };
-	    keys = {
-
-	    	'37': {
-	    		allowEvent: true,
-
-	    		down: function(){
-	    			StickMan.walk(-1);
-	    		}
-	    		up: function(){
-	    			stickMan.stopWalk();
-	    		}
-	    	}
-	    	'39':{
-	    		allowEvent: true,
-
-	    		down: function(){
-	    			stickMan.walk(1);
-	    		}
-	    		up: function(){
-	    			stickMan.stopWalk();
-	    		}
-	    	}
-	    	'38':{
-	    		allowEvent: true,
-
-	    		down: function(){
-	    			stickMan.jump();
-	    		}
-	    	}
-
-	    }
-	   function isDown(labelOrCode){
-	   	return (typeof keyMap[labelOrCode] !== 'undefined' ? downKeys[keyMap[labelOrCode]] : downKeys[labelOrCode]);
-	   }
-	   function releaseAll() {
-
-      
-    	  var item;
-      		for (item in downKeys) {
-        		if (downKeys.hasOwnProperty(item)) {
-		          events.keyup({
-        		    keyCode: item
-        		  });
-       			 }
-      		}
-
-		    }
-
-		    function attachEvents() {
-
-		      utils.events.add(document, 'keydown', events.keydown);
-		      utils.events.add(document, 'keyup', events.keyup);
-
-		    }
-
-		    
-
-		    function init() {
-
-		      attachEvents();
-
-		    }
-
-		    return {
-
-		      init: init,
-		      isDown: isDown,
-		      releaseAll: releaseAll
-
-		    };
+		keyMap = {
+			'left' : 37,
+			'up' : 38,
+			'right' : 39
+		},
+		events = {
+			KeyDown: function(e){
+				if(keys[e.Keycode]&&keys[e.Keycode].down){ 
+					if (typeof downKeys[e.keyCode] === 'undefined') {
+						downKeys[e.keyCode] = true;
+						keys[e.keyCode].down(e);
+					}
+					if (typeof keys[e.keyCode].allowEvent === 'undefined') {
+						return stopEvent(e);
+					}
+				}
+			},
+			keyup: function(e) {
+				if (typeof downKeys[e.keyCode] !== 'undefined' && keys[e.keyCode]) {
+					delete downKeys[e.keyCode];
+					if (keys[e.keyCode].up) {
+						keys[e.keyCode].up(e);
+					}
+					if (typeof keys[e.keyCode].allowEvent === 'undefined') {
+						return stopEvent(e);
+					}
+				}
 			}
-}
-function init(){
-	objects.KeyBoardMonitor = new KeyBoardMonitor
-	objects.KeyBoardMonitor.init(),
-	objects.platforms = new platform(map);
-}
-function gameLoop(){
-	//these coulod be used to activate walking animation and delay gravity while jumping
-	/*css{
-	'phase0': ' '
-	'phase1': 'phase1'
-	'phase2': 'phase2'
-	'phase3': 'phase3'
-	'phase4': 'phase4'
-	}*/
-	data = {
-		frameTimer: null,
-		frameIntervals: 50;
-	}
-	events= {
-		loop: function(){
-			game.objects.StickMan.animate();
+		};
+		keys = {
+			'37': {
+				allowEvent: true,
+				down: function(){
+					StickMan.walk(-1);
+				},
+				up: function(){
+					stickMan.stopWalk();
+				}
+			},
+			'39':{
+				allowEvent: true,
+				down: function(){
+					stickMan.walk(1);
+			},
+				up: function(){
+					stickMan.stopWalk();
+				}
+			},
+			'38':{
+				allowEvent: true,
+				down: function(){
+					stickMan.jump();
+				}
+			}
+		};
+		function isDown(labelOrCode){
+			return (typeof keyMap[labelOrCode] !== 'undefined' ? downKeys[keyMap[labelOrCode]] : downKeys[labelOrCode]);
 		}
-		startFrames: function(){
-			data.frameIntervals = window.setInterval(events.loop, data.frameIntervals);
+		function releaseAll() {
+			var item;
+			for (item in downKeys) {
+				if (downKeys.hasOwnProperty(item)) {
+					events.keyup({
+						keyCode: item
+					});
+				}
+			}
 		}
+		function attachEvents() {
+			utils.events.add(document, 'keydown', events.keydown);
+			utils.events.add(document, 'keyup', events.keyup);
+		}   
+		function init() {
+			attachEvents();
+		}
+		return {
+			init: init,
+			isDown: isDown,
+			releaseAll: releaseAll
+		};
 	}
+	function init(){
+		objects.KeyBoardMonitor = new KeyBoardMonitor();
+		objects.KeyBoardMonitor.init(),
+		objects.platforms = new platform(map);
+	}
+	function gameLoop(){
+		//these coulod be used to activate walking animation and delay gravity while jumping
+		/*css{
+		'phase0': ' '
+		'phase1': 'phase1'
+		'phase2': 'phase2'
+		'phase3': 'phase3'
+		'phase4': 'phase4'
+		}*/
+		data = {
+			frameTimer: null,
+			frameIntervals: 50
+		};
+		events= {
+			loop: function(){
+				game.objects.StickMan.animate();
+			},
+			startFrames: function(){
+				data.frameIntervals = window.setInterval(events.loop, data.frameIntervals);
+			}
+		};
+	}
+	return game.objects.platforms;
+};
+
+function start(){
+	Game = new easterEgg();
+	Game.init();
 }
---
+++ symbol-game/test.js
@@ -0,0 +1,16 @@
+describe("easterEgg", function(){
+	it("1+1 should equal 2", function(){
+		expect(Game.game.objects.platforms[0][1]).toBe(15)
+	});
+});
+/*
+	it("map to platform array", function(){
+		
+		it("platform array correct", function(){
+			expect(game.objects.platforms).toBe([[0,15],[5,15],[10,15],[30,35],[35,35],[40,35],[0,50],[50,5],[50,10],[15,50],[20,50],[25,50],[30,50],[35,50],[40,50],[45,50]]);
+		});
+		it("platform array is not correct", function(){
+			expect(game.objects.platforms).not.toBe([[0,15],[5,15],[10,15],[30,35],[35,35],[40,35],[0,50],[50,5],[50,10],[15,50],[20,50],[25,50],[30,50],[35,50],[40,50],[45,50]]);
+		});
+	});
+});*/
